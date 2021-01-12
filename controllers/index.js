7d1e3b72084b17aa4fb59bc93251d6c539923b90
sxiong6901 <soua.p.xiong@hotmail.com>
2021-01-11 21:13:31 -0500

    deploy3

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const jwtDecode = require('jwt-decode')
-const User = require('../models/user.js')
+const User = require('../models/User.js')
 const jokes = require('../data')
 const signToken = require('../auth').signToken
 
